6b02e0902cfa3eedc2cb51140b3ed4e803d06edb
reaperhound <reaperhound69004@gmail.com>
2023-04-07 00:49:09 +0530

    created the CategoryList and CategoryItem component they all display images

---
+++ src/components/category-list/category-list.component.jsx
@@ -0,0 +1,15 @@
+import CategoryItem from "../category-item/category-item.component";
+import "./category-list.styles.scss";
+
+const CategoryList = ({ categories }) => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => {
+        const { id } = category;
+        return <CategoryItem key={id} category={category} />;
+      })}
+    </div>
+  );
+};
+
+export default CategoryList;
